85ea6e6907b67e4f93849cf7bae9f7a195c63510
Megan Luu <megluu04@gmail.com>
2025-02-25 18:55:06 -0800

    Added persona page

---
+++ src/pages/MeganPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MegPage = () => {
+    return (
+        <div>
+            <h1>Welcome to Megan's Page</h1>
+            <p>blah blah blah</p>
+            <Link to="/">Back to home page</Link>
+        </div>
+    )
+};
+
+export default MegPage
